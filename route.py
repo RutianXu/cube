97eda5d02b72c0a6cf9d6fde38bf088f3237d21d
Tim Xu <140560519+RutianXu@users.noreply.github.com>
2024-05-02 10:46:35 +1200

    added some templates but code is not working

---
+++ route.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return '(:'
+
+
+@app.route('/about')
+def about():
+    return':('
+
+
+@app.route('/algorithms/<algorithm_set>')
+def algorithm(algorithm_set):
+    conn = sqlite3.connect('cube.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM algorithms WHERE algorithm_set=?', (algorithm_set,))
+    alg = cur.fetchone()
+    return render_template('algorithms.html', alg=alg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
