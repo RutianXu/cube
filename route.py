c4e4a792daf382d5f6c73dbc1592f3ecda738cdd
Tim Xu <140560519+RutianXu@users.noreply.github.com>
2024-05-09 11:19:25 +1200

    image and algorithms now show up in the website

--- route.py
+++ route.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import sqlite3
+import base64
 
 app = Flask(__name__)
 
@@ -20,6 +21,11 @@ def algorithm(algorithm_set):
     cur.execute('SELECT * FROM algorithms WHERE algorithm_set=?',(algorithm_set,))
     alg = cur.fetchall()
     conn.close()
+    alg = [list(item) for item in alg]
+    for algorithm in alg:
+        image_blob = algorithm[-1]  
+        encoded_image = base64.b64encode(image_blob).decode('utf-8')
+        algorithm[-1] = encoded_image
     return render_template('algorithms.html', alg=alg)
 
 if __name__ == '__main__':
