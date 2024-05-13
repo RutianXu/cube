ac07d2c25881fd942187fad64589b012a5d17031
Tim Xu <140560519+RutianXu@users.noreply.github.com>
2024-05-13 12:26:25 +1200

    algorithms can display in the website
    multiple algorithms that solves the same case can display in the website together with the same images

--- route.py
+++ route.py
@@ -19,14 +19,22 @@ def algorithm(algorithm_set):
     conn = sqlite3.connect('cube.db')
     cur = conn.cursor()
     cur.execute('SELECT * FROM algorithms WHERE algorithm_set=?',(algorithm_set,))
-    alg = cur.fetchall()
+    algorithms = cur.fetchall()
     conn.close()
-    alg = [list(item) for item in alg]
-    for algorithm in alg:
+
+    algorithms = [list(item) for item in algorithms]
+    alg=[]
+    algs=[]
+    for algorithm in algorithms:
         image_blob = algorithm[-1]  
-        encoded_image = base64.b64encode(image_blob).decode('utf-8')
-        algorithm[-1] = encoded_image
-    return render_template('algorithms.html', alg=alg)
+        if image_blob != None:
+            encoded_image = base64.b64encode(image_blob).decode('utf-8')
+            algorithm[-1] = encoded_image
+            alg.append(algorithm)
+        else:
+            algs.append(algorithm)
+            
+    return render_template('algorithms.html', alg=alg, algs=algs,)
 
 if __name__ == '__main__':
     app.run(debug=True)
