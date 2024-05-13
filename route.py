adec783de9f1363fee0f1ce9808070be2ee32180
Tim Xu <140560519+RutianXu@users.noreply.github.com>
2024-05-14 10:50:53 +1200

    added a navigation bar and home page
    the links to the homepage and the algorithms page are working

--- route.py
+++ route.py
@@ -6,12 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return '(:'
-
-
-@app.route('/about')
-def about():
-    return':('
+    return render_template('home.html')
 
 
 @app.route('/algorithms/<algorithm_set>')
